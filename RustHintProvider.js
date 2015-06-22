823716b191e8c2d864e036dab44252ac83261c8d
Rrandom <emanonhere@gmail.com>
2015-06-22 11:13:12 +0800

    outline of hint utils

--- RustHintProvider.js
+++ RustHintProvider.js
@@ -1,16 +1,126 @@
+/**
+ *
+ * Licensed under MIT
+ *
+ * Modified from https://github.com/David5i6/Brackets-Go-IDE
+ *
+ * Docs to finish this file:
+ *  https://codemirror.net/doc/manual.html
+ *  https://github.com/adobe/brackets/wiki/Brackets-Node-Process:-Overview-for-Developers
+ *
+ */
+
+
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
 /*global define, $, brackets */
 
 define(function (require, exports, module) {
     'use strict';
 
-    console.log("entering rusthintprovider");
+    var ExtensionUtils = brackets.getModule('utils/ExtensionUtils'),
+        NodeDomain = brackets.getModule('utils/NodeDomain'),
+        NodeConnection = brackets.getModule('utils/NodeConnection');
+
+    var RustHinterDomain;
+    var nodeConnection = new NodeConnection();
+    var connection = nodeConnection.connect(true);
+
+    connection.done(function () {
+        RustHinterDomain = new NodeDomain('RustHinter',
+            ExtensionUtils.getModulePath(module, "node/RustHinterDomain"));
+    });
+
+    //
+    var $deferred, cm, vpet = 0;
+
+    // TO-DO
+    function validToken(implicitChar) {
+        return false;
+    }
+
+    // TO-DO
+    function getLastToken() {
+        return 'i';
+    }
+
+    function getHintsD(txt, cursor) {
+        $deferred = new $.Deferred();
+        console.info('Call RustHinterDomain');
+        // TO-DO: cursor -> linenum, charnum
+        RustHinterDomain.exec("getHint", txt, cursor, ++vpet)
+            .fail(function (err) {
+                console.error('[RustHinterDomain] Fail to get hints: ', err);
+            });
+        return $deferred;
+
+    }
+
+    // TO-DO
+    function formatHints(data) {
+        return data;
+    }
 
 
     function RustHintProvider() {
-        this.hasHints = function (editor, implicitChar) {};
-        this.getHints = function (implicitChar) {};
-        this.insertHint = function (hint) {};
+
+
+
+        function resolveHint(data, petition) {
+            if (petition === vpet) {
+                $deferred.resolve({
+                    hints: formatHints(data),
+                    match: '',
+                    selectInitial: true,
+                    handleWideResults: false
+                });
+            }
+        }
+
+
+
+        this.hasHints = function (editor, implicitChar) {
+            cm = editor._codeMirror;
+            return validToken(implicitChar);
+        };
+
+        this.getHints = function (implicitChar) {
+            console.info('Asking Hints');
+
+            if (validToken(implicitChar)) {
+                // cursor: {line, ch}
+                var cursor = cm.getCursor(),
+                    txt = cm.getValue();
+                return getHintsD(txt, cursor);
+            } else {
+                return false;
+            }
+
+        };
+
+        this.insertHint = function ($hint) {
+            if (!$hint) {
+                throw new TypeError("Must provide valid hint and hints object as they are returned by calling getHints");
+            } else {
+                // TO-DO: Consider these two lines, maybe don't need it
+                var lasttoken = getLastToken();
+                cm.replaceSelection($hint.data('token').substring(lasttoken.length));
+            }
+        };
+
+
+        /**
+         * When domain send the update event (and only if there're no multiple version events...
+         * Format the response and resolve the promise.
+         */
+        $(nodeConnection).on("RustHinter:update", function (evt, data, petition) {
+            if (petition === vpet) {
+                if (data === 'PANIC PANIC PANIC\n') {
+                    data = '';
+                }
+                console.info('#### On update event, data: ' + data);
+                resolveHint(data, petition);
+            }
+        });
     }
 
     return RustHintProvider;
---
+++ node/RustHinterDomain.js
@@ -0,0 +1,120 @@
+/*
+ * Copyright (c) 2012 Adobe Systems Incorporated. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+
+/*
+ * Docs to finish this file:
+ * https://github.com/adobe/brackets/wiki/Brackets-Node-Process:-Overview-for-Developers
+ * https://nodejs.org/api/child_process.html
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50,node: true */
+/*global define, $, brackets */
+
+(function () {
+    "use strict";
+
+
+    var _domainManager,
+        spawn = require('child_process').spawn,
+        exec = require('child_process').exec,
+        extName = '[rust-ide]',
+        racerPath = 'D:\\tmp\\emacs\\racer\\target\\release\\racer.exe';
+
+
+    // TO-DO: hints cache
+    // TO-DO: call racer correctly
+    /**
+     * @private
+     * call outside racer
+     * @param txt {string} current edit buffer/
+     * @param linenum {number}
+     * @param charnum {number}
+     * @param petition {number}
+     */
+    function cmdGetHint(txt, linenum, charnum, petition) {
+        console.info('cmdGetHint --> ');
+        try {
+            var racer = spawn(racerPath, ['complete',
+                                          linenum,
+                                         charnum,
+                                          //TO-DO
+                                         'tmp.racertmp']);
+            var tmp = '';
+
+            racer.stdin.write(txt);
+            racer.stdin.end();
+
+            racer.stdout.on('data', function (data) {
+                tmp += data.toString();
+            });
+
+            racer.stderr.on('data', function (data) {
+                console.info(extName + 'stderr: ' + data);
+            });
+
+            racer.on('close', function (code) {
+                _domainManager.emitEvent('RustHinter', 'update', [tmp, petition]);
+            });
+
+
+        } catch (e) {
+            console.error(extName + e);
+        }
+    }
+
+    /**
+     * Initializes the domain.
+     * @param {DomainManager} domainManager The DomainManager for the server
+     */
+    function init(domainManager) {
+        console.info('Rust NodeDomain init');
+        if (!domainManager.hasDomain("RustHinter")) {
+            domainManager.registerDomain("RustHinter", {
+                major: 0,
+                minor: 1
+            });
+        }
+
+        //TO-DO
+        domainManager.registerCommand(
+            "RustHinter",
+            "getHint",
+            cmdGetHint,
+            false,
+            "Return Rust Hints", [], []
+        );
+
+        //TO-DO
+        domainManager.registerEvent(
+            "RustHinter",
+            "update", []
+        );
+
+        _domainManager = domainManager;
+
+    }
+
+    exports.init = init;
+
+}());
