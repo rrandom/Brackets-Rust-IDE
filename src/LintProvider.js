806416b227a70e6792cc154230a5cb4e8c17d819
Rrandom <emanonhere@gmail.com>
2015-11-21 10:05:33 +0800

    `NodeDomain` instead of  `NodeConnection`

--- src/LintProvider.js
+++ src/LintProvider.js
@@ -7,22 +7,32 @@ define(function (require, exports, module) {
         DocumentManager = brackets.getModule('document/DocumentManager'),
         CodeInspection = brackets.getModule('language/CodeInspection'),
         EditorManager = brackets.getModule('editor/EditorManager'),
-        NodeConnection = brackets.getModule('utils/NodeConnection'),
+        NodeDomain = brackets.getModule("utils/NodeDomain"),
         ExtensionUtils = brackets.getModule('utils/ExtensionUtils'),
-        ProjectManager = brackets.getModule('project/ProjectManager'),
-        node = new NodeConnection();
+        ProjectManager = brackets.getModule('project/ProjectManager');
 
     var pattern = /^(.+?):(\d+):(\d+):\s+(\d+):(\d+)\s(error|fatal error|warning):\s+(.+)/;
 
-    if (!node.domains.rustlint) {
-        node.connect(true).done(function () {
-            var path = ExtensionUtils.getModulePath(module, 'node/LintDomain.js');
-            node.loadDomains([path], true).done(function () {
-                AppInit.appReady(init);
+    var _domainPath = ExtensionUtils.getModulePath(module, 'node/LintDomain');
+
+    var _nodeDomain = new NodeDomain('rustLint', _domainPath);
+
+    // TO-DO: `--lib or --bin NAME`
+    function getLintErrors(filePath, useCargo, manifest) {
+        var errors,
+            deferred = new $.Deferred(),
+            cmd = useCargo ? 'cargo rustc -Zno-trans --manifest-path ' + manifest : 'rustc -Z no-trans ' + filePath;
+
+        _nodeDomain.exec('getLint', cmd)
+            .done(function (data) {
+                errors = parserError(data, filePath);
+                deferred.resolve(errors);
+            }).fail(function (err) {
+                console.error('RustLint: ', err);
+                deferred.reject(null);
             });
-        });
-    } else {
-        AppInit.appReady(init);
+
+        return deferred.promise();
     }
 
     function normalizePath(path) {
@@ -53,23 +63,7 @@ define(function (require, exports, module) {
             });
     }
 
-    // TO-DO: `--lib or --bin NAME`
-    function getLintErrors(filePath, useCargo, manifest) {
-        var errors,
-            deferred = new $.Deferred(),
-            cmd = useCargo ? 'cargo rustc -Zno-trans --manifest-path ' + manifest : 'rustc -Z no-trans ' + filePath;
-
-        node.domains.rustlint.commander(cmd)
-            .done(function (data) {
-                errors = parserError(data, filePath);
-                deferred.resolve(errors);
-            }).fail(function (err) {
-                console.error('RustLint: ', err);
-                deferred.reject(null);
-            });
-
-        return deferred.promise();
-    }
+    // ------ gutter ---
 
     function registerGutter() {
         var currentEditor = EditorManager.getActiveEditor(),
@@ -102,6 +96,8 @@ define(function (require, exports, module) {
             addMarkers(cm, errors);
         }
     }
+
+    // ---- end gutter ----
 
     var useCargo,
         codeMirror,
@@ -187,4 +183,6 @@ define(function (require, exports, module) {
         ProjectManager.on('projectOpen', projectOpenHandler);
         EditorManager.on('activeEditorChange', activeEditorChangeHandler);
     }
+
+    AppInit.appReady(init);
 });
--- src/node/LintDomain.js
+++ src/node/LintDomain.js
@@ -1,23 +1,29 @@
 (function () {
+    "use strict";
 
-    var child_process;
+    var child_process = require('child_process');
 
-    child_process = require('child_process');
-
-    exports.init = function (manager) {
-        if (!manager.hasDomain('rustlint'))
-            manager.registerDomain('rustlint', {
-                major: 1,
-                minor: 0
-            });
-
-        manager.registerCommand('rustlint', 'commander', commander, true);
-    };
-
-    function commander(exec, cb) {
+    function _cmdGetLint(exec, cb) {
         child_process.exec(exec, function (err, stdout, stderr) {
             cb(null, stderr + stdout);
         });
     }
+
+    function init(manager) {
+        if (!manager.hasDomain('rustLint'))
+            manager.registerDomain('rustLint', {
+                major: 1,
+                minor: 0
+            });
+
+        manager.registerCommand(
+            'rustLint',
+            'getLint',
+            _cmdGetLint,
+            true // true then `cb()`, false then `return`
+        );
+    }
+
+    exports.init = init;
 
 }());
